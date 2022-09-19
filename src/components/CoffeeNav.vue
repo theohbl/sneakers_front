21d9b892e23ac5fdbb374536db60476a809c3445
theohbl <theo.houlbert3@gmail.com>
2022-09-19 16:41:55 +0200

    commit

--- src/components/CoffeeNav.vue
+++ src/components/CoffeeNav.vue
@@ -1,29 +1,28 @@
 <template>
-    <div>
-        <h1 class="site-heading text-center text-white d-none d-lg-block">
-          <span class="site-heading-lower">Sneakers</span>
-        </h1>
-        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-          <div class="container">
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav mx-auto">
-                <li class="nav-item active px-lg-4">
-                  <router-link class="nav-link text-uppercase text-expanded" :to="{ name: 'Home' }">Home</router-link>
-                </li>
-                <li class="nav-item px-lg-4">
-                    <router-link class="nav-link text-uppercase text-expanded router-link-exact-path-active" :to="{ name: 'About' }">About</router-link>
-                </li>
-                <li class="nav-item px-lg-4">
-                  <router-link class="nav-link text-uppercase text-expanded" :to="{ name: 'products' }">Products</router-link>
-                </li>
-                <li class="nav-item px-lg-4">
-                  <router-link class="nav-link text-uppercase text-expanded" :to="{ name: 'Store' }">Store</router-link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-    </div>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <router-link class="nav-link text-uppercase text-expanded" :to="{ name: 'Home' }">Home</router-link>
+            </li>
+            <li class="nav-item px-lg-4">
+              <router-link class="nav-link text-uppercase text-expanded router-link-exact-path-active"
+                :to="{ name: 'About' }">About</router-link>
+            </li>
+            <li class="nav-item px-lg-4">
+              <router-link class="nav-link text-uppercase text-expanded" :to="{ name: 'products' }">Products
+              </router-link>
+            </li>
+            <li class="nav-item px-lg-4">
+              <router-link class="nav-link text-uppercase text-expanded" :to="{ name: 'Store' }">Store</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
